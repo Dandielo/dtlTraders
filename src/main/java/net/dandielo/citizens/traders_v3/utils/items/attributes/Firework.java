8668729605877dac34b297a149824ba5363f2fe4
dandielo <daniel.penkala@gmail.com>
2013-05-25 03:25:19 +0200

    Firework support soon

--- src/main/java/net/dandielo/citizens/traders_v3/utils/items/attributes/Firework.java
+++ src/main/java/net/dandielo/citizens/traders_v3/utils/items/attributes/Firework.java
@@ -1,5 +1,41 @@
 package net.dandielo.citizens.traders_v3.utils.items.attributes;
 
-public class Firework {
+import org.bukkit.inventory.ItemStack;
+
+import net.dandielo.citizens.traders_v3.core.exceptions.InvalidItemException;
+import net.dandielo.citizens.traders_v3.core.exceptions.attributes.AttributeInvalidValueException;
+import net.dandielo.citizens.traders_v3.core.exceptions.attributes.AttributeValueNotFoundException;
+import net.dandielo.citizens.traders_v3.utils.items.Attribute;
+import net.dandielo.citizens.traders_v3.utils.items.ItemAttr;
+
+@Attribute(name="Firework", key="fw")
+public class Firework extends ItemAttr {
+
+	public Firework(String key)
+	{
+		super(key);
+	}
+
+	@Override
+	public void onLoad(String data) throws AttributeInvalidValueException
+	{
+	}
+
+	@Override
+	public String onSave()
+	{
+		return null;
+	}
+
+	@Override
+	public void onAssign(ItemStack item) throws InvalidItemException
+	{
+	}
+
+	@Override
+	public void onFactorise(ItemStack item)
+			throws AttributeValueNotFoundException
+	{
+	}
 
 }
