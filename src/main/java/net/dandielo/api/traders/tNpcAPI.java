a24c95d8fb16b317246d477425a83a4629b53de2
dandielo <daniel.penkala@gmail.com>
2013-05-30 23:37:42 +0200

    API clean up

--- src/main/java/net/dandielo/api/traders/tNpcAPI.java
+++ src/main/java/net/dandielo/api/traders/tNpcAPI.java
@@ -1,12 +1,8 @@
 package net.dandielo.api.traders;
 
-import java.util.Collection;
-
-import net.dandielo.citizens.traders_v3.tNpc;
 import net.dandielo.citizens.traders_v3.tNpcManager;
 
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 
 public class tNpcAPI {
 	private static tNpcManager manager = tNpcManager.instance(); 
@@ -20,24 +16,6 @@ public class tNpcAPI {
 	 */
 	public static boolean isTNpcInventory(Player player)
 	{
-		return tNpcManager.instance().tNpcInventoryOpened(player);
-	}
-	
-	/*
-	 * Test methods
-	 */
-	public static Collection<Inventory> getInventories()
-	{
-		return tNpcManager.instance().getInventories();
-	}
-	
-	public static boolean inRelation(Player player)
-	{
-		return manager.inRelation(player);
-	}
-	
-	public static tNpc getRelation(Player player)
-	{
-		return manager.getRelation(player.getName(), tNpc.class);
+		return manager.tNpcInventoryOpened(player);
 	}
 }
