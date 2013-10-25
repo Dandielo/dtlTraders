5a7e63019b4ce96cadc19e1e6e0c2377f939f0a8
dandielo <daniel.penkala@gmail.com>
2013-10-25 03:08:30 +0200

    Back to old GUI behavior

--- src/main/java/net/dandielo/citizens/traders_v3/utils/items/attributes/Price.java
+++ src/main/java/net/dandielo/citizens/traders_v3/utils/items/attributes/Price.java
@@ -16,9 +16,9 @@ import net.dandielo.citizens.traders_v3.utils.items.ItemAttr;
 
 @Attribute(
 name="Price", key = "p", standalone = true, priority = 0,
-status = {tNpcStatus.BUY, tNpcStatus.SELL, tNpcStatus.SELL_AMOUNTS, tNpcStatus.MANAGE_PRICE, tNpcStatus.MANAGE_BUY, tNpcStatus.MANAGE_SELL})
+status = {tNpcStatus.BUY, tNpcStatus.SELL, tNpcStatus.SELL_AMOUNTS, tNpcStatus.MANAGE_PRICE})
 public class Price extends ItemAttr {
-	public static String lorePattern = ChatColor.GOLD + "Price: " + ChatColor.GRAY;
+    public static String lorePattern = ChatColor.GOLD + "Price: " + ChatColor.GRAY;
 	private double price;
 
 	public Price(String key) {
@@ -77,15 +77,35 @@ public class Price extends ItemAttr {
 		throw new AttributeValueNotFoundException();
 	}
 	
+
 	@Override
 	public void onStatusLoreRequest(tNpcStatus status, ItemStack target, List<String> lore)
 	{
-	    if (status == tNpcStatus.MANAGE_BUY || status == tNpcStatus.MANAGE_SELL || status == tNpcStatus.MANAGE_PRICE) {
-	        for ( String pLore : LocaleManager.locale.getLore("item-unitPrice") )
-	            lore.add(pLore.replace("{price}", String.format("%.2f", price)).replace(',', '.'));
-	    }
+		//If not in manager mode then we don't want to manage this request
+		//Maybe later just update the Attribute settings?
+		if ( !status.inManagementMode() ) return;
+
+		//add the lore to the item
+		for ( String pLore : LocaleManager.locale.getLore("item-unitPrice") )
+			lore.add(pLore.replace("{price}", String.format("%.2f", price)).replace(',', '.'));
 	}
 	
+	//This is here ONLY because I KNOW that when I need to debug anything 
+	//price related I will look in the Price Attribute automatically
+	public static List<String> loreRequest(double price, List<String> lore, tNpcStatus status)
+	{
+		if ( price < 0 ) return lore;
+		
+		//add the Price lore
+		for ( String pLore : LocaleManager.locale.getLore("item-price") )
+			lore.add(pLore.replace("{price}", String.format("%.2f", price)).replace(',', '.'));
+
+		//add additional click info lore
+		lore.addAll(LocaleManager.locale.getLore("item-" + status));
+		return lore;
+	}
+	
+	/*
 	public static List<String> loreRequest(double price, List<String> lore, tNpcStatus status)
 	{
 		if ( price < 0 ) return lore;
@@ -99,5 +119,5 @@ public class Price extends ItemAttr {
     		lore.addAll(LocaleManager.locale.getLore("item-" + status));
 		}
 		return lore;
-	}
+	}*/
 }
